c0049d6f8665e06237aef093221b002ce458ee41
Chetan M <meshram.chetan@gmail.com>
2021-06-05 14:22:33 -0700

    add navigation to 404 page

--- portfolio/src/pages/404.js
+++ portfolio/src/pages/404.js
@@ -1,7 +1,10 @@
 import React from "react"
 
+import Header from "../components/header"
+
 const NotFoundPage = () => (
   <div>
+    <Header />
     <h1>404: Not Found</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </div>
